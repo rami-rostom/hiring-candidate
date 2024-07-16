ffc1b875dd03134fa2cdf60aff75fda686506a59
Rami <rami.rostom@proton.me>
2024-07-16 21:27:03 +0200

    :sparkles: feat: edit header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,7 @@ function Header() {
       <a href="https://www.dataleon.ai/" target='_blank' className='header__logo'>
         <img src={logo} alt='Logo Dataleon'/>
       </a>
-
       <h1 className='header__title'>Hiring Quiz</h1>
-
       <a href="https://github.com/rami-rostom" target='_blank' className='header__name'>
         <p>Rami Rostom</p>
       </a>
