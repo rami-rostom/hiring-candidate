6f559a0784004f53e24fb01206da4d064c44f3ca
Rami <rami.rostom@proton.me>
2024-07-16 17:30:17 +0200

    :sparkles: feat: fetch data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
+
 import './styles/App.css';
 
 function App() {
+  const [data, setData] = useState('');
+
+  // Fetch datas from json file
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('No data found', error));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
