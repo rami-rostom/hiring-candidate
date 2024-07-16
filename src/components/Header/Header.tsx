78b218a2b32477dc0803cc1a46532c3988e9d090
Rami <rami.rostom@proton.me>
2024-07-16 21:20:22 +0200

    :sparkles: feat: edit header style + create reset style file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import logo  from '../../assets/dataleon.svg';
+import './Header.css';
 
 function Header() {
   return (
    <>
     <header className='header'>
-      <img src={logo} alt='Logo Dataleon' />
-      <h1>Hiring Quiz</h1>
+      <a href="https://www.dataleon.ai/" target='_blank' className='header__logo'>
+        <img src={logo} alt='Logo Dataleon'/>
+      </a>
+
+      <h1 className='header__title'>Hiring Quiz</h1>
+
+      <a href="https://github.com/rami-rostom" target='_blank' className='header__name'>
+        <p>Rami Rostom</p>
+      </a>
     </header>
    </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+
+import './styles/reset.css';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(
