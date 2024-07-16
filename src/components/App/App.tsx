325fe7658220e6bebefe366175197f055ebdcf36
Rami <rami.rostom@proton.me>
2024-07-16 20:43:52 +0200

    :sparkles: feat: add response button feature + show right answer depending of button click

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,7 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState<DataState>();
+  const [answer, setAnswer] = useState<string>();
 
   // Fetch datas from json file
   useEffect(() => {
@@ -15,6 +16,20 @@ function App() {
       .catch((error) => console.error('No data found', error));
   }, []);
 
+  // Handle response of the question if user say YES
+  const handleAnswerYes = () => {
+    if (data) {
+      setAnswer(data.answers[0]);
+    }
+  }
+
+  // Handle response of the question if user say NO
+  const handleAnswerNo = () => {
+    if (data) {
+      setAnswer(data.answers[1]);
+    }
+  }
+
   return (
     <div className='app'>
       {/* Import header component */}
@@ -22,6 +37,17 @@ function App() {
 
       <main className='game'>
         <h2 className="game__question">{data?.question}</h2>
+        <div className="game__answers">
+          <button onClick={handleAnswerYes} className="game__answers-button">
+            Yes
+          </button>
+          <button onClick={handleAnswerNo} className="game__answers-button">
+            No
+          </button>
+        </div>
+        <div className="game__result">
+          <h3>{answer}</h3>
+        </div>
       </main>
     </div>
   );
